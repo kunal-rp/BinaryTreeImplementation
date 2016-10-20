e30a7feced01d7bdf6d9aab55b81a4ce85e4f589
kunal-rp <kunal.r.rajpurohit@gmail.com>
2016-10-20 14:03:58 -0700

    Added in 'delete' functionality to node -Will now work on BST search for node function

--- src/com/BinaryTree/Project1/PurohitKp011877606.java
+++ src/com/BinaryTree/Project1/PurohitKp011877606.java
@@ -51,6 +51,21 @@ public class PurohitKp011877606 {
 	public int getvalue(){
 		return value;
 	}
+	public void deleteNode(){
+
+		if(right != null){
+			Node p = this;
+			Node q = right;
+			while(q.getLeft() != null){
+				p = q;
+				q = q.getLeft();
+			}
+			value = p.getvalue();
+			p.setLeft(q.right);
+
+		}
+
+	}
 	
 }
 
